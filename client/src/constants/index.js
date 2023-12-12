2276a37217b46396155c28883667cb99017d7f95
ranahmadasif <110496500+ranahmadasif@users.noreply.github.com>
2023-12-12 16:28:16 +0500

    Delete client directory

--- client/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const options = [
-    {title:"Category", list: ["miscellaneous", "teaser", "trailer", "reaction", "comedy", "interview"]},
-    {title:"Visibility", list: ["public", "private"]},
-    // {title:"Quality"},
-    // // {title:"Others"}
-]
